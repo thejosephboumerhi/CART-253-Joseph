494561545a606acaaa99974058a6c585b291cfdc
Joseph Boumerhi <joeyboumerhi@gmail.com>
2020-09-20 20:41:52 -0400

    Started 03 - Moving Pictures

---
+++ Activities/03-Moving Pictures/js/script.js
@@ -0,0 +1,21 @@
+/**************************************************
+02 - Moving Picture
+
+Joseph Boumerhi
+
+The picture moves around 
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}

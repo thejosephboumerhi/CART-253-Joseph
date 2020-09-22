0ddf470c0598edc63ab1bdfa6bc967c3ba603a57
Joseph Boumerhi <joeyboumerhi@gmail.com>
2020-09-21 22:59:54 -0400

    E1: Started exercise 1

---
+++ Exercises/Exercise1 - Move it, Move it/js/script.js
@@ -0,0 +1,17 @@
+/**************************************************
+Exercise 1 - I Like To Move It, Move It
+Joseph Boumerhi
+
+It does...
+**************************************************/
+
+function setup() {
+createCanvas (500,500);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}

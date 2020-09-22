c1649da2655f4ce1bb19353b3e5893d38406b726
Joseph Boumerhi <joeyboumerhi@gmail.com>
2020-09-22 19:56:47 -0400

    E1: Circle map and constrains work properly, added a random flickering for the square. Added more comments, and organized a bit more. Finished.
    It went well, and was fun to make. Got help from Samuel (TA), so it could be good and function well.

--- Exercises/Exercise1 - Move it, Move it/js/script.js
+++ Exercises/Exercise1 - Move it, Move it/js/script.js
@@ -2,10 +2,11 @@
 Exercise 1 - I Like To Move It, Move It
 Joseph Boumerhi
 
-It does...
+Responds to mouseX and Y, has vertical movement based on mouseY,size change based on the distance of mouseX,
 **************************************************/
 //Variables associated with shapes
 
+//Middle rectangle
 let rectan = {
   x: 250,
   y: 250,
@@ -14,6 +15,7 @@ let rectan = {
 
 };
 
+//Left circle
 let circle1 = {
   x: 500,
   y: 250,
@@ -21,6 +23,7 @@ let circle1 = {
 
 };
 
+//Right circle
 let circle2 = {
   x: 0,
   y: 250,
@@ -28,54 +31,47 @@ let circle2 = {
 
 };
 
+//Just a black background
 let bg = {
-  r: 255,
+  r: 0,
   g: 0,
   b: 0
 
 };
 
-let fg = {
-  r:0,
-  g:0,
-  b:0
-
-};
-
-
-//Sets up canvas
+//Sets up canvas, and Frame Rate to 60.
 function setup() {
 createCanvas (500,500);
 frameRate (60);
 }
 
-//For every frame,
+//Runs the program, draws and alters shapes.
 function draw() {
 background(bg.r,bg.g,bg.b);
 
+//Random flikering foreground
+fill(random(0,225));
 square(width/2,height/2,450);
 
-
-
-//The rectangle moves along the Y axis with mouseY
+//The rectangle moves along the Y axis with mouseY.
+//Changes into different shades of green, up is darker, down is lighter.
 fill(0,mouseY,0);
-rect(rectan.x,rectan.y,rectan.w,rectan.h);
-rectan.y = constrain(mouseY,0,500);
 rectMode(CENTER);
+rectan.y = constrain(mouseY,0,500);
+rect(rectan.x,rectan.y,rectan.w,rectan.h);
 
-//Ellipses that get bigger the higher mouseX is, and vice-versa
+//Ellipses that get bigger when mouseX is closer, and vice-versa.
+//Also, the closer, the more darker red, and the further, the lighter the red.
 //Left ellipse
-fill(mouseX,0,0);
+fill(width-mouseX,0,0);
 circle1.size = map(width - mouseX,0,width,50,175);
+circle1.size = constrain(circle1.size,50,175);
 ellipse(circle1.x,circle1.y,circle1.size);
-circle1.size = constrain(mouseX,50,175);
 
 //Right ellipse
-fill(50);
+fill(mouseX,0,0);
 circle2.size = map(mouseX,0,width,50,175);
+circle2.size = constrain(circle2.size,50,175);
 ellipse(circle2.x,circle2.y,circle2.size);
-circle2.size = constrain(mouseX,50,175);
 
-
-console.log ("mouseX"+mouseX,"mouseY"+mouseY);
 }

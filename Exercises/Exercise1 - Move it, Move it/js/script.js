84d2adec2b709be78a350531d794c30518960fbd
Joseph Boumerhi <joeyboumerhi@gmail.com>
2020-09-22 16:28:37 -0400

    E1: Added constrain for rectan, preparing to map the circle1 and circle2 with size

--- Exercises/Exercise1 - Move it, Move it/js/script.js
+++ Exercises/Exercise1 - Move it, Move it/js/script.js
@@ -6,48 +6,58 @@ It does...
 **************************************************/
 //Variables associated with shapes
 
-let square = {
+let rectan = {
   x: 250,
   y: 250,
   w: 175,
-  h: 175,
+  h: 175
 
 };
 
 let circle1 = {
   x: 500,
   y: 250,
-  size:250
+  size:50
 
 };
 
 let circle2 = {
   x: 0,
   y: 250,
-  size:250
+  size:50
 
 };
 
+let bg = {
+  r: 255,
+  g:0,
+  b:0
+
+};
 
 
 //Sets up canvas
 function setup() {
 createCanvas (500,500);
+frameRate (60);
 }
 
-// draw()
-//
-// Description of draw() goes here.
+//For every frame,
 function draw() {
-background(0);
+background(bg.r,bg.g,bg.b);
 fill(255);
 
-
-rect(square.x,square.y,square.w,square.h);
+//The rectangle moves along the Y axis
+rect(rectan.x,rectan.y,rectan.w,rectan.h);
+rectan.y = constrain(mouseY,0,500);
 rectMode(CENTER);
 
+//Left ellipse
 ellipse(circle1.x,circle1.y,circle1.size);
 
+//Right ellipse
 ellipse(circle2.x,circle2.y,circle2.size);
 
+
+console.log ("mouseX"+mouseX,"mouseY"+mouseY);
 }

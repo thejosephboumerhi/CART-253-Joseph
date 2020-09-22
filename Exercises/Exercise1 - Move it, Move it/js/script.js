e3f028a13014ea3f6ade0f75afe59a6290947239
Joseph Boumerhi <joeyboumerhi@gmail.com>
2020-09-21 23:29:45 -0400

    E1: Setup three shapes, some space for comments and additional code

--- Exercises/Exercise1 - Move it, Move it/js/script.js
+++ Exercises/Exercise1 - Move it, Move it/js/script.js
@@ -4,7 +4,33 @@ Joseph Boumerhi
 
 It does...
 **************************************************/
+//Variables associated with shapes
 
+let square = {
+  x: 250,
+  y: 250,
+  w: 175,
+  h: 175,
+
+};
+
+let circle1 = {
+  x: 500,
+  y: 250,
+  size:250
+
+};
+
+let circle2 = {
+  x: 0,
+  y: 250,
+  size:250
+
+};
+
+
+
+//Sets up canvas
 function setup() {
 createCanvas (500,500);
 }
@@ -13,5 +39,15 @@ createCanvas (500,500);
 //
 // Description of draw() goes here.
 function draw() {
+background(0);
+fill(255);
+
+
+rect(square.x,square.y,square.w,square.h);
+rectMode(CENTER);
+
+ellipse(circle1.x,circle1.y,circle1.size);
+
+ellipse(circle2.x,circle2.y,circle2.size);
 
 }

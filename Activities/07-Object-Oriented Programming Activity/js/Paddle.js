c900863344c24e2760015458a618370b5ff26b29
Joseph Boumerhi <joeyboumerhi@gmail.com>
2020-10-27 20:39:00 -0400

    A7: Finished A7

--- Activities/07-Object-Oriented Programming Activity/js/Paddle.js
+++ Activities/07-Object-Oriented Programming Activity/js/Paddle.js
@@ -1,1 +1,20 @@
-class Paddle {}
+class Paddle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+    this.x = 0;
+    this.y = height - this.height / 2;
+  }
+  move() {
+    this.x = mouseX;
+  }
+
+  display() {
+    push();
+    fill(255);
+    noStroke();
+    rectMode(CENTER);
+    rect(this.x, this.y, this.width, this.height);
+    pop();
+  }
+}
--- Activities/07-Object-Oriented Programming Activity/js/script.js
+++ Activities/07-Object-Oriented Programming Activity/js/script.js
@@ -1,20 +1,43 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity 07 - Object-Oriented Programming Activity
+Joseph Boumerhi
 
-Here is a description of this template p5 project.
+Simulates juggling via paddle
 **************************************************/
+"use: strict";
 
-// setup()
-//
-// Description of setup() goes here.
+let state = "title";
+
+let gravityForce = 0.0025;
+let paddle;
+
+let balls = [];
+let numBalls = 3;
+
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  paddle = new Paddle(300, 20);
 
+  for (i = 0; i < numBalls; i++) {
+    let x = random(0, width);
+    let y = random(-400, -100);
+    let ball = new Ball(x, y);
+    balls.push(ball);
+  }
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
+  background(0);
+  paddle.move();
+  paddle.display();
 
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    if (ball.active) {
+      ball.gravity(gravityForce);
+      ball.move();
+      ball.bounce(paddle);
+      ball.display();
+    }
+  }
 }

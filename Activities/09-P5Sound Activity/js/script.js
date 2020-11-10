79fbe6c3e182ea2ea184849130e4e598507a040f
Joseph Boumerhi <joeyboumerhi@gmail.com>
2020-11-10 03:45:03 -0500

    A9: Finished Sound Activity

--- Activities/09-P5Sound Activity/js/script.js
+++ Activities/09-P5Sound Activity/js/script.js
@@ -1,18 +1,37 @@
-/**************************************************
+/*****************************************************************
 09-P5 Sound Activity
 Joseph Boumerhi
 
-Here is a description of this template p5 project.
-**************************************************/
+Simple Sound Activity, plays sound(s), could be used for music(?)
+*****************************************************************/
+let balls = [];
 
-// setup()
-//
-// Description of setup() goes here.
+//For Music Notes
+let notes = [`F3`, `G3`, `Ab4`, `Bb4`, `C4`, `Dd4`, `Eb4`, `F4`];
+
+// Puts up the canvas
 function setup() {
-  background(150, 55, 90);
+  createCanvas(600, 600);
+  userStartAudio();
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {}
+function draw() {
+  background(30);
+
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+}
+
+function mousePressed() {
+  createBall(mouseX, mouseY);
+}
+
+function createBall(x, y) {
+  let note = random(notes);
+  let ball = new Ball(x, y, note);
+  balls.push(ball);
+}

5da45e19557fa79b05159871c875d56b085b5988
Joseph Boumerhi <joeyboumerhi@gmail.com>
2020-11-10 01:06:41 -0500

    A9: Started Sound Activity

---
+++ Activities/09-P5Sound Activity/js/script.js
@@ -0,0 +1,18 @@
+/**************************************************
+09-P5 Sound Activity
+Joseph Boumerhi
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  background(150, 55, 90);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {}

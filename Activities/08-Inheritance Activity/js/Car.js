549b6a7b22e584517f50bd180b60b09e17b7daa0
Joseph Boumerhi <joeyboumerhi@gmail.com>
2020-11-10 21:15:04 -0500

    A8: Finished Inheritance Activity

---
+++ Activities/08-Inheritance Activity/js/Car.js
@@ -0,0 +1,18 @@
+class Car extends Vehicle {
+  constructor(x, y) {
+    super(x, y);
+    this.width = 50;
+    this.height = 20;
+    this.speed = 5;
+  }
+
+  display() {
+    super.display();
+    push();
+    rectMode(CENTER);
+    fill(255, 0, 0);
+    noStroke();
+    rect(this.x, this.y, this.width, this.height);
+    pop();
+  }
+}
--- Activities/08-Inheritance Activity/js/script.js
+++ Activities/08-Inheritance Activity/js/script.js
@@ -1,20 +1,112 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity 08 - Inheritance Activity
+Joseph Boumerhi
 
-Here is a description of this template p5 project.
+Inheritance stuff, Pedestrian Palaver
 **************************************************/
+let state = `simulation`;
 
-// setup()
-//
-// Description of setup() goes here.
+let pedestrian;
+
+let vehicles = [];
+let numCars = 10;
+let numTrucks = 10;
+let numMotorcycles = 5;
+
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  let x = width / 2;
+  let y = height;
+  pedestrian = new Pedestrian(x, y);
+
+  for (let i = 0; i < numCars; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let car = new Car(x, y);
+    vehicles.push(car);
+  }
+  for (let i = 0; i < numTrucks; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let truck = new Truck(x, y);
+    vehicles.push(truck);
+  }
+  for (let i = 0; i < numMotorcycles; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let motorcycle = new Motorcycle(x, y);
+    vehicles.push(motorcycle);
+  }
+
+  for (let i = 0; i < vehicles.length; i++) {
+    let r = random(0, 1);
+    let vehicle = vehicles[i];
+    if (r < 0.5) {
+      vehicle.vx = -vehicle.speed;
+    } else {
+      vehicle.vx = vehicle.speed;
+    }
+  }
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
+  background(0);
+  if (state === `title`) {
+    title();
+  } else if (state === `simulation`) {
+    simulation();
+  } else if (state === `success`) {
+    success();
+  } else if (state === `dead`) {
+    dead();
+  }
+}
 
+function simulation() {
+  pedestrian.handleInput();
+  pedestrian.move();
+  pedestrian.display();
+
+  if (!pedestrian.alive) {
+    state = `dead`;
+  }
+  if (pedestrian.y < 0) {
+    state = `success`;
+  }
+
+  for (let i = 0; i < vehicles.length; i++) {
+    let vehicle = vehicles[i];
+    vehicle.move();
+    vehicle.wrap();
+    vehicle.display();
+    pedestrian.chechHit(vehicle);
+  }
+}
+
+function title() {
+  displayText(`Pedestrian Palaver`);
+}
+
+function success() {
+  displayText(`You Made It!`);
+}
+
+function dead() {
+  displayText(`You Are Dead!`);
+}
+
+function displayText(string) {
+  pop();
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  fill(255);
+  text(string, width / 2, height / 2);
+  push();
+}
+
+function keyPressed() {
+  if (state === `title`) {
+    state = `simulation`;
+  }
 }
